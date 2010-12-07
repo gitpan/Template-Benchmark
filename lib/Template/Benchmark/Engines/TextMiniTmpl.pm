7121d653726dbf8da55ac387bd43595f9cb23ebb
Sam Graham <sgraham@cpan.org>
2010-12-07 22:13:23 +0000

    Import of SGRAHAM/Template-Benchmark-1.09_01 from CPAN.
    gitpan-cpan-distribution: Template-Benchmark
    gitpan-cpan-version:      1.09_01
    gitpan-cpan-path:         SGRAHAM/Template-Benchmark-1.09_01.tar.gz
    gitpan-cpan-author:       SGRAHAM
    gitpan-cpan-maturity:     developer

--- lib/Template/Benchmark/Engines/TextMiniTmpl.pm
+++ lib/Template/Benchmark/Engines/TextMiniTmpl.pm
@@ -1,13 +1,11 @@
-package Template::Benchmark::Engines::TextTemplate;
+package Template::Benchmark::Engines::TextMiniTmpl;
 
 use warnings;
 use strict;
 
 use base qw/Template::Benchmark::Engine/;
 
-use Text::Template;
-
-use File::Spec;
+use Text::MiniTmpl 1.1.0 qw( render tmpl2code raw );
 
 our $VERSION = '1.09_01';
 
@@ -15,54 +13,75 @@ our %feature_syntaxes = (
     literal_text              =>
         join( "\n", ( join( ' ', ( 'foo' ) x 12 ) ) x 5 ),
     scalar_variable           =>
-        '{$scalar_variable}',
+        '#~ $_{scalar_variable} ~#',
     hash_variable_value       =>
-        '{$hash_variable{hash_value_key}}',
+        '#~ $_{hash_variable}->{hash_value_key} ~#',
     array_variable_value      =>
-        '{$array_variable[ 2 ]}',
+        '#~ $_{array_variable}->[ 2 ] ~#',
     deep_data_structure_value =>
-        '{$this{is}{a}{very}{deep}{hash}{structure}}',
+        '#~ $_{this}->{is}{a}{very}{deep}{hash}{structure} ~#',
     array_loop_value          =>
-        '{ $OUT .= $_ foreach @array_loop; }',
+        '&~foreach ( @{$_{array_loop}} ) {~&' .
+        '#~ $_ ~#' .
+        '&~}~&',
     hash_loop_value           =>
-        '{ $OUT .= "$_: $hash_loop{$_}" foreach sort( keys( %hash_loop ) ); }',
+        '&~foreach ( sort( keys( %{$_{hash_loop}} ) ) ) {~&' .
+        '#~ $_ ~#: #~ $_{hash_loop}->{$_} ~#' .
+        '&~}~&',
     records_loop_value        =>
-        '{ $OUT .= "$_->{name}: $_->{age}" foreach @records_loop; }',
+        '&~foreach ( @{$_{records_loop}} ) {~&' .
+        '#~ $_->{ name } ~#: #~ $_->{ age } ~#' .
+        '&~}~&',
     array_loop_template       =>
-        undef,
+        '&~foreach ( @{$_{array_loop}} ) {~&' .
+        '#~ $_ ~#' .
+        '&~}~&',
     hash_loop_template        =>
-        undef,
+        '&~foreach ( sort( keys( %{$_{hash_loop}} ) ) ) {~&' .
+        '#~ $_ ~#: #~ $_{hash_loop}->{$_} ~#' .
+        '&~}~&',
     records_loop_template     =>
-        undef,
+        '&~foreach ( @{$_{records_loop}} ) {~&' .
+        '#~ $_->{ name } ~#: #~ $_->{ age } ~#' .
+        '&~}~&',
     constant_if_literal       =>
-        '{ if( 1 ) { $OUT .= "true"; } }',
+        '&~if( 1 ) {~&true&~}~&',
     variable_if_literal       =>
-        '{ if( $variable_if ) { $OUT .= "true"; } }',
+        '&~if( $_{variable_if} ) {~&true&~}~&',
     constant_if_else_literal  =>
-        '{ if( 1 ) { $OUT .= "true"; } else { $OUT .= "false"; } }',
+        '&~if( 1 ) {~&true&~} else {~&' .
+        'false&~}~&',
     variable_if_else_literal  =>
-        '{ if( $variable_if_else ) { $OUT .= "true"; } else ' .
-        '{ $OUT .= "false"; } }',
+        '&~if( $_{variable_if_else} ) {~&true&~} ' .
+        'else {~&false&~}~&',
     constant_if_template      =>
-        undef,
+        '&~if( 1 ) {~&' .
+        '#~ $_{template_if_true} ~#&~}~&',
     variable_if_template      =>
-        undef,
+        '&~if( $_{variable_if} ) {~&' .
+        '#~ $_{template_if_true} ~#&~}~&',
     constant_if_else_template =>
-        undef,
+        '&~if( 1 ) {~&' .
+        '#~ $_{template_if_true} ~#&~} ' .
+        'else {~&' .
+        '#~ $_{template_if_false} ~#&~}~&',
     variable_if_else_template =>
-        undef,
+        '&~if( $_{variable_if_else} ) {~&' .
+        '#~ $_{template_if_true} ~#&~} ' .
+        'else {~&' .
+        '#~ $_{template_if_false} ~#&~}~&',
     constant_expression       =>
-        '{10 + 12}',
+        '#~ 10 + 12 ~#',
     variable_expression       =>
-        '{$variable_expression_a * $variable_expression_b}',
+        '#~ $_{variable_expression_a} * $_{variable_expression_b} ~#',
     complex_variable_expression =>
-        '{ ( ( $variable_expression_a * $variable_expression_b ) + ' .
-        '$variable_expression_a - $variable_expression_b ) / ' .
-        '$variable_expression_b }',
+        '#~ ( ( $_{variable_expression_a} * $_{variable_expression_b} ) + ' .
+        '$_{variable_expression_a} - $_{variable_expression_b} ) / ' .
+        '$_{variable_expression_b} ~#',
     constant_function         =>
-        q[{substr( 'this has a substring.', 11, 9 )}],
+        q[#~ substr( 'this has a substring.', 11, 9 ) ~#],
     variable_function         =>
-        '{substr( $variable_function_arg, 4, 2 )}',
+        '#~ substr( $_{variable_function_arg}, 4, 2 ) ~#',
     );
 
 sub syntax_type { return( 'embedded-perl' ); }
@@ -71,8 +90,10 @@ sub pure_perl { return( 1 ); }
 sub benchmark_descriptions
 {
     return( {
-        TeTe    =>
-            "Text::Template ($Text::Template::VERSION)",
+        TMT    =>
+            "Text::MiniTmpl ($Text::MiniTmpl::VERSION)",
+        TMTU   =>
+            "Text::MiniTmpl ($Text::MiniTmpl::VERSION) with enabled Unicode",
         } );
 }
 
@@ -80,17 +101,7 @@ sub benchmark_functions_for_uncached_string
 {
     my ( $self ) = @_;
 
-    return( {
-        TeTe =>
-            sub
-            {
-                my $t = Text::Template->new(
-                    TYPE              => 'STRING',
-                    SOURCE            => $_[ 0 ],
-                    );
-                \$t->fill_in( HASH => [ $_[ 1 ], $_[ 2 ] ] );
-            },
-        } );
+    return( undef );
 }
 
 sub benchmark_functions_for_uncached_disk
@@ -98,14 +109,21 @@ sub benchmark_functions_for_uncached_disk
     my ( $self, $template_dir ) = @_;
 
     return( {
-        TeTe =>
+        TMT =>
             sub
             {
-                my $t = Text::Template->new(
-                    TYPE   => 'FILE',
-                    SOURCE => File::Spec->catfile( $template_dir, $_[ 0 ] ),
-                    );
-                \$t->fill_in( HASH => [ $_[ 1 ], $_[ 2 ] ] );
+                raw(1);
+                tmpl2code( $template_dir . '/' . $_[ 0 ] )->(
+                    %{$_[ 1 ]}, %{$_[ 2 ]},
+                )
+            },
+        TMTU =>
+            sub
+            {
+                raw(0);
+                tmpl2code( $template_dir . '/' . $_[ 0 ] )->(
+                    %{$_[ 1 ]}, %{$_[ 2 ]},
+                )
             },
         } );
 }
@@ -134,18 +152,25 @@ sub benchmark_functions_for_memory_cache
 sub benchmark_functions_for_instance_reuse
 {
     my ( $self, $template_dir, $cache_dir ) = @_;
-    my ( $t );
+    my ( $t, $out );
 
     return( {
-        TeTe =>
+        TMT =>
             sub
             {
-                $t = Text::Template->new(
-                    TYPE   => 'FILE',
-                    SOURCE => File::Spec->catfile( $template_dir, $_[ 0 ] ),
-                    )
-                    unless $t;
-                \$t->fill_in( HASH => [ $_[ 1 ], $_[ 2 ] ] );
+                raw(1);
+                \render( $template_dir . '/' . $_[ 0 ],
+                    %{$_[ 1 ]}, %{$_[ 2 ]},
+                )
+            },
+        TMTU =>
+            sub
+            {
+                raw(0);
+                # use ../../ to force different paths/caches for TMT and TMTU
+                \render( ('../' x 20) . $template_dir . '/' . $_[ 0 ],
+                    %{$_[ 1 ]}, %{$_[ 2 ]},
+                )
             },
         } );
 }
@@ -158,17 +183,19 @@ __END__
 
 =head1 NAME
 
-Template::Benchmark::Engines::TextTemplate - Template::Benchmark plugin for Text::Template.
+Template::Benchmark::Engines::TextMiniTmpl - Template::Benchmark plugin for Text::MiniTmpl.
 
 =head1 SYNOPSIS
 
 Provides benchmark functions and template feature syntaxes to allow
-L<Template::Benchmark> to benchmark the L<Text::Template> template
+L<Template::Benchmark> to benchmark the L<Text::MiniTmpl> template
 engine.
 
 =head1 AUTHOR
 
 Sam Graham, C<< <libtemplate-benchmark-perl at illusori.co.uk> >>
+
+Alex Efros, C<< <powerman-asdf@ya.ru> >>
 
 =head1 BUGS
 
@@ -180,7 +207,7 @@ automatically be notified of progress on your bug as I make changes.
 
 You can find documentation for this module with the perldoc command.
 
-    perldoc Template::Benchmark::Engines::TextTemplate
+    perldoc Template::Benchmark::Engines::TextMiniTmpl
 
 
 You can also look for information at:
@@ -214,7 +241,7 @@ inspiration for this module.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2010 Sam Graham.
+Copyright 2010 Sam Graham & Alex Efros.
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of either: the GNU General Public License as published
